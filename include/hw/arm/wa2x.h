4b6e9e9de18daf953cb15d6a755ab418c5609e5d
12101111 <w12101111@gmail.com>
2026-04-28 21:09:32 +0800

    Fix header path change in 11.0

--- include/hw/arm/wa2x.h
+++ include/hw/arm/wa2x.h
@@ -20,7 +20,7 @@
 #define HW_WA2X_H
 
 #include "target/arm/cpu.h"
-#include "hw/boards.h"
+#include "hw/core/boards.h"
 #include "hw/char/wa2x_syscon.h"
 
 #define TYPE_ARM_WA2X_MACHINE MACHINE_TYPE_NAME("wa2x")
--- include/hw/riscv/wa2x.h
+++ include/hw/riscv/wa2x.h
@@ -20,7 +20,7 @@
 #define HW_WA2X_H
 
 #include "hw/riscv/riscv_hart.h"
-#include "hw/boards.h"
+#include "hw/core/boards.h"
 #include "hw/char/wa2x_syscon.h"
 
 #define TYPE_RISCV_WA2X_MACHINE MACHINE_TYPE_NAME("wa2x")
